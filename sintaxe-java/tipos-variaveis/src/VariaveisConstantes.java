e637ef0194ef9f39a94791a37696e9fc678da191
Tunnex <tunnexca@gmail.com>
2023-02-15 23:06:03 -0300

    Aula Tipos e Variáveis

---
+++ sintaxe-java/tipos-variaveis/src/VariaveisConstantes.java
@@ -0,0 +1,14 @@
+public class VariaveisConstantes {
+    
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_PI = 3.14; //por convenção, o nome de constantes são escritos em caixa alta
+
+        //VALOR_PI = 3.15; 
+
+        System.out.print(VALOR_PI);
+    }
+}
