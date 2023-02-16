acd3943ff0566120e6af307eb44ea546a259c504
Tunnex <tunnexca@gmail.com>
2023-02-15 23:06:34 -0300

    Aula tipos e Variáveis

---
+++ sintaxe-java/tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,12 @@
+public class TipoVariaveis {
+    
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //não é possível fazer o tipo short referir-se ao tipo int
+
+        
+    }
+}
